86621dba32c99bc16fe7344d5b48a7947b44e52a
kimigor57 <71986660+kimigor57@users.noreply.github.com>
2020-12-15 15:58:19 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,100 +1,116 @@
-#include <math.h>
+#include <stdio.h>;
+#include <math.h>;
 #include <malloc.h>
-#include "stdio.h"
 
 
-float inputArray(float* array, int size);
-float group(float* array, int size);
-float bubbleSort(float* array, int size);
-float outputArray(float* array, int size);
+int** allocate(int lines, int columns);
+void inputMatrix(int** matrix, int lines, int columns);
+void outputMatrix(int** matrix, int lines, int columns);
+void transport(int** matrix, int lines, int columns);
+void swapMinMax(int** matrix, int lines, int columns);
+void tallMatrix(int** matrix, int lines, int  columns, int square);
+void longMatrix(int** matrix, int lines, int columns, int square);
+void squaredMatrix(int** matrix, int lines, int columns, int square);
+
+
 int main()
 {
+	int lines, number, distance, columns;
+	int** matrix;
+	printf("input number of lines:\n");
+	scanf_s("%d", &lines);
+	printf("input number of columns:\n");
+	scanf_s("%d", &columns);
 
-	int size;
-	printf("Insert number of elements:\n");
-	scanf_s("%d", &size);
-	float* array = (float*)malloc(size * sizeof(float));
-	inputArray(array, size);
-	bubbleSort(array, size);
-	group(array, size);
-	outputArray(array, size);
+	allocate( lines, columns);
+	matrix= allocate(lines, columns);
+	inputMatrix(matrix, lines, columns);
+	outputMatrix(matrix, lines, columns);
+	transport( matrix,lines,columns);
+	outputMatrix(matrix, lines, columns);	
 	getchar();
 	getchar();
 }
 
-float inputArray(float* array, int size)
+int** allocate(int lines, int columns)
 {
-	for (int i = 0; i < size; i = i + 1)
+	int* array = (int*) malloc(lines * columns * sizeof(int));
+	int** matrix = (int**)malloc(lines * sizeof(int*));
+	for (int i = 0; i < lines; i++)
 	{
-		scanf_s("%f", &array[i]);
+		matrix[i] = array + i * columns;
 	}
-	return 0;
+	return matrix;
 }
-
-float bubbleSort(float *array, int size)
+void inputMatrix(int** matrix, int lines, int columns)
 {
- 
-  for (int i = 0; i < size - 1; i++)
-  {
-    for (int j = (size - 1); j > i; j--) 
-    {
-      if (array[j - 1] > array[j]) 
-      {
-        int temp = array[j - 1]; 
-        array[j - 1] = array[j];
-        array[j] = temp;
-      }
-    }
-  }
-  return 0;
-}
-
-float group(float* array, int size)
-{
-	int k=0;
-	for (int i = 0; i < size; i = i + 1)
+	for (int i = 0; i < lines; i++) 
 	{
-		if (array[i] < 0)
-			k=k+1;
-	}
-	for (int i = size - 1; i >= 0; i--)
-	{
-		if (array[i] < 0)
+		for (int j = 0; j < columns; j++) 
 		{
-			float tmp;
-			float tmp2 = array[i];
-			float tmp1 = array[size - 1];
-			for (int j = size - 2; j >= i; j--)
-			{
-				tmp = array[j];
-				array[j] = tmp1;
-				tmp1 = tmp;
-			}
-			array[size - 1] = tmp2;
+			scanf_s("%d", &matrix[i][j]);
 		}
+		printf("\n");
 	}
-	for (int i = size-k; i < size - 1; i++)
-    {
-		for (int j = (size - 1); j > i; j--) 
-		{
-			if (array[j - 1] > array[j]) 
-			{
-				int temp = array[j - 1]; 
-				array[j - 1] = array[j];
-				array[j] = temp;
-			}
-		}
-    }
-	return 0;
 }
-
-
-
-float outputArray(float* array, int size)
+void outputMatrix(int** matrix, int lines, int columns)
 {
-	for (int i = 0; i < size; i = i + 1)
+	for (int i = 0; i < lines; i++) 
 	{
-		printf("%.0f ", array[i]);
+		for (int j = 0; j < columns; j++) 
+		{
+			printf("%d", matrix[i][j]);
+		}
+		printf("\n");
 	}
-	return 0;
 }
+void tallMatrix(int** matrix, int lines, int  columns, int square)
+{
+	
+	for (int i = square; i < lines; ++i)
+		for (int j = 0; j < columns - i + square; ++j)
+		{
+			int tmp = matrix[i][j];
+			matrix[i][j] = matrix[lines - j - 1][columns - i - 1 + square];
+			matrix[lines - j - 1][columns - i - 1 + square] = tmp;
+		}
+	
+}
+void longMatrix(int** matrix, int lines, int columns, int square)
+{
+	for (int i = 0; i < lines; ++i)
+		for (int j = 0; j < columns - i - square; ++j)
+		{
+			int tmp = matrix[i][j];
+			matrix[i][j] = matrix[lines - j - 1][columns - i - 1 - square];
+			matrix[lines - j - 1][columns - i - 1 - square] = tmp;
+		}
+}
+void squaredMatrix(int** matrix, int lines, int columns, int square)
+{
+	for (int i = 0; i < lines; ++i)
+		for (int j = 0; j < columns - i; ++j)
+		{
+			int tmp = matrix[i][j];
+			matrix[i][j] = matrix[lines - j - 1][columns - i - 1];
+			matrix[lines - j - 1][columns - i - 1] = tmp;
+		}
+}
+void transport(int** matrix, int lines, int columns)
+{
+	int square = abs(lines - columns);
+	if (lines < columns)
+	{
+		longMatrix(matrix, lines, columns, square);
+	}
+
+	else if (lines > columns) 
+	{
+		tallMatrix(matrix, lines, columns, square);
+	}
+	else
+	{
+		squaredMatrix(matrix, lines, columns, square);	
+	}
+}
+
